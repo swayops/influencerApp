e6e0810c73357975e6d51b1341eb9b5cb8dea0da
OneOfOne <oneofone@gmail.com>
2016-10-17 03:53:46 +0200

    allow login

--- app/login.cmp.ts
+++ app/login.cmp.ts
@@ -9,12 +9,18 @@ import { Sway, HasAPI } from './sway';
 	templateUrl: './views/login.html',
 })
 export class LoginCmp extends HasAPI {
+	public loading = false;
+	public data: any = {};
 	constructor(title: Title, public api: Sway) {
 		super(api);
 		title.setTitle('Login');
 	}
 
 	Login(evt: Event) {
-
+		this.loading = true;
+		this.api.Login(this.data, err => {
+			this.loading = false;
+			this.AddNotification('error', err.msg);
+		});
 	}
 }
