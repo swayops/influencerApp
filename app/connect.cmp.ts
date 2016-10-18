91c922a07fd1f4c2579399fc7e19aadca23cd8f5
OneOfOne <oneofone@gmail.com>
2016-10-18 20:03:19 +0200

    connect page

--- app/connect.cmp.ts
+++ app/connect.cmp.ts
@@ -9,8 +9,30 @@ import { Sway, HasAPI } from './sway';
 	templateUrl: './views/connect.html',
 })
 export class ConnectCmp extends HasAPI {
+	public data: any;
+	public loading = false;
 	constructor(title: Title, public api: Sway) {
 		super(api);
 		title.setTitle('Connect Your Social Accounts');
+		const inf = this.user.inf || {};
+		this.data = {
+			instagram: inf.instagram && inf.instagram.id,
+			youtube: inf.youtube && inf.youtube.id,
+			twitter: inf.twitter && inf.twitter.id,
+			facebook: inf.facebook && inf.facebook.id,
+		};
+	}
+
+	LinkAccounts() {
+		this.loading = true;
+		for (let [k, v] of Object.entries(this.data)) {
+			if (!!v) {
+				this.api.Get('setPlatform/' + this.user.id + '/' + k + '/' + v,
+					data => { /* */ },
+					err => this.AddNotification('error', err.msg)
+				);
+			}
+		}
+		this.api.GoTo('/dealFeed');
 	}
 }
