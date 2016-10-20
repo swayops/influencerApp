83545cdca68985be860d37adbf7a4781ad12b41d
OneOfOne <oneofone@gmail.com>
2016-10-20 23:23:31 +0200

    cashout earnings

--- app/cashoutEarnings.cmp.ts
+++ app/cashoutEarnings.cmp.ts
@@ -10,10 +10,26 @@ import { Sway, HasAPI } from './sway';
 	templateUrl: './views/cashoutEarnings.html',
 })
 export class CashoutEarningsCmp extends HasAPI {
-	private ts: number;
 	constructor(title: Title, public api: Sway, route: ActivatedRoute) {
 		super(api);
 		title.setTitle('Cashout Earnings');
-		this.ts = parseInt(route.snapshot.params['ts']);
+	}
+
+	get amount(): number {
+		return this.user.inf.pendingPayout || 0;
+	}
+
+	Cashout() {
+		if (this.amount === 0) {
+			this.api.GoTo('earningStats');
+			return;
+		}
+
+		this.api.Get('requestCheck/' + this.user.id, resp => {
+			this.AddNotification('info', 'Check your email for the documents!');
+			this.api.GoTo('earningStats');
+		}, err => {
+			this.AddNotification('error', err.msg);
+		});
 	}
 }
