e556d0e25c9ef4e4b985309e13056933d82b5417
OneOfOne <oneofone@gmail.com>
2017-02-03 21:46:40 +0200

    confirm addr for dealDetail

--- app/dealDetail.cmp.ts
+++ app/dealDetail.cmp.ts
@@ -1,35 +1,48 @@
 // DealDetail
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { Title } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
 
-import { Sway, HasAPI } from './sway';
+import { HasAPI, Sway } from './sway';
+import { InfInfo } from './utils';
 
 @Component({
 	selector: 'dealDetail',
 	templateUrl: './views/dealDetail.html',
 })
 export class DealDetailCmp extends HasAPI {
+	public confirmButtons = [
+		{ name: 'Confirm', class: 'btn-primary', click: (_) => this.Accept(), disabledIf: () => this.info.missing },
+		{ name: 'Edit Profile', class: 'btn-default ghost', click: (_) => this.api.GoTo('/editProfile') },
+	];
+
 	public deal: any;
 	public err: any;
+	private info_: any;
+
 	constructor(title: Title, public api: Sway, route: ActivatedRoute) {
 		super(api);
 		title.setTitle('Deal Details');
 		const cmpID = route.snapshot.params['cid'],
 			dealID = route.snapshot.params['id'];
 
-		this.api.Get('getDeal/' + this.user.id + '/' + cmpID + '/' + dealID, data => {
+		this.api.Get('getDeal/' + this.user.id + '/' + cmpID + '/' + dealID, (data) => {
 			this.deal = data;
-		}, err => this.err = err.msg);
+		}, (err) => this.err = err.msg);
+	}
+
+	get info(): any {
+		if (!this.info_) this.info_ = InfInfo(this.user);
+		return this.info_ || {};
 	}
 
 	Accept() {
 		const d = this.deal,
 			uid = this.user.id;
 		// /assignDeal/:influencerId/:campaignId/:dealId/:platform
-		this.api.Get('assignDeal/' + uid + '/' + d.campaignId + '/' + d.id + '/' + d.platforms[0], data => {
+		this.api.Get('assignDeal/' + uid + '/' + d.campaignId + '/' + d.id + '/' + d.platforms[0], (data) => {
 			this.SetData('deal:' + data.assigned, data);
 			this.api.GoTo('acceptedDealAlert', data.assigned);
-		}, err => this.err = err.msg);
+		}, (err) => this.err = err.msg);
 	}
 }
