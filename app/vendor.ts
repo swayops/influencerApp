f2280e34c2678c5841224ef2fd20f97d4db2e0db
OneOfOne <oneofone@gmail.com>
2017-06-02 20:03:51 +0200

    version pump

--- app/vendor.ts
+++ app/vendor.ts
@@ -1,13 +1,12 @@
-import 'core-js/client/shim.min';
 import 'reflect-metadata';
 import 'zone.js/dist/zone.min';
 
-import 'rxjs/Observable';
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/take';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/take';
+import 'rxjs/Observable';
 
 import 'ng2-img-cropper';
