d236adc4f1cf781fac9f9fe032655d66c448bc30
OneOfOne <oneofone@gmail.com>
2017-07-11 22:33:12 +0200

    remove tax stuff

--- app/cashoutEarnings.cmp.ts
+++ app/cashoutEarnings.cmp.ts
@@ -38,29 +38,16 @@ export class CashoutEarningsCmp extends HasAPI {
 			this.api.GoTo('earningStats');
 			return;
 		}
-		if (this.user.inf.hasSigned) {
-			this.api.Get('requestCheck/' + this.user.id, (resp) => {
-				this.AddNotification('info', `
+		this.api.Get('requestCheck/' + this.user.id, (resp) => {
+			this.AddNotification('info', `
 You've successfully requested a check be sent to your address.
 Please wait up to 48 hours for an admin to approve your payment and a check will be on it's way.
 You will receive an email once the admin has approved.
 `);
-				this.api.GoTo('earningStats');
-			}, (err) => {
-				this.AddNotification('error', err.msg);
-			});
-		} else {
-			this.api.Get('emailTaxForm/' + this.user.id, (resp) => {
-				this.AddNotification('success', `
-Tax forms have been emailed to you. Please check your spam folder as well.
-Once these have been completed you will be eligible for payment.
-`);
-				this.api.GoTo('earningStats');
-			}, (err) => {
-				this.AddNotification('error', err.msg);
-			});
-		}
+			this.api.GoTo('earningStats');
+		}, (err) => {
+			this.AddNotification('error', err.msg);
+		});
+
 	}
-
-
 }
